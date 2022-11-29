9f008946a295ffa63ba48e32835fd7fdc4d17afa
israelvallejos <78510655+israelvallejos@users.noreply.github.com>
2022-11-28 22:22:33 -0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Exer.Classes
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ EmployeeBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Exer.Classes
+{
+
+    public class EmployeeBL
+    {
+        public IEmployeeDAL employeeDAL;
+        public EmployeeBL(IEmployeeDAL employeeDAL)
+        {
+            this.employeeDAL = employeeDAL;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeDAL.SelectAllEmployees();
+        }
+    }
+}
